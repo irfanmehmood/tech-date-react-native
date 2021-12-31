36921290a5ebe48a78934ffb54e7e29f3f8becca
Irfan Mehmood <irfmehmood@googlemail.com>
2021-12-31 13:25:37 +0000

    Added country flag for profiles

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,22 +2,41 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { styles } from "../Libs/style";
 
-const Card = ({match}) => {
+const Card = ({ match }) => {
   return (
-    <View style={(styles.card, {borderRadius:10, backgroundColor: "transparent", opacity: 1,})}>
+    <View
+      style={
+        (styles.card,
+        { borderRadius: 10, backgroundColor: "transparent", opacity: 1 })
+      }
+    >
       <Image
-        style={{ height: "95%", borderRadius:15, marginBottom:-7 }}
-    
+        style={{ height: "75%", borderRadius: 15, marginBottom: -7 }}
         name="user"
         source={{
           uri: match.imageUrl,
         }}
       />
 
-      <View style={{borderBottomLeftRadius: 15 , borderBottomRightRadius: 15 ,padding:10, marginTop: -75,backgroundColor: "black", opacity: 0.75}}>
+      <View
+        style={{
+          borderBottomLeftRadius: 15,
+          borderBottomRightRadius: 15,
+          padding: 10,
+          marginTop: -75,
+          backgroundColor: "black",
+          opacity: 0.75,
+        }}
+      >
         <Text style={styles.cardText}>{match.name}</Text>
         <Text style={styles.cardText}>Age: {match.age}</Text>
         <Text style={styles.cardText}>Country: {match.countryCode}</Text>
+        <Image
+          style={{ height: 32, width: 64, margin: 5 }}
+          source={{
+            uri: `https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/png250px/${match.countryCode.toLowerCase()}.png`,
+          }}
+        />
       </View>
     </View>
   );
